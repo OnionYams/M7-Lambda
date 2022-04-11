5262d17d5031e3676aac858faa25c5e77273113d
Y Tang <ytangw03854@gmail.com>
2022-04-11 16:30:14 -0500

    New repo and improvements

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # M7-Lambda
+
+Using filter and lambda and reduce to get select information and summary stats about a dataset of detroit 911 calls
--- lambda_map_reduce.py
+++ lambda_map_reduce.py
@@ -1,33 +1,49 @@
 
-import csv
+#took inspiration from Jakob's model
+
+import csv, json
 from functools import reduce
 
-f = open("911_Calls_for_Service_(Last_30_Days).csv", newline='')
-reader = csv.DictReader(f)
+calls = open("911_Calls_for_Service_(Last_30_Days).csv", newline='')
+callsDict = csv.DictReader(calls)
     
 filteredCalls = list(filter(lambda row: row["zip_code"] != "" and row["neighborhood"] != "" 
-                            and row["totalresponsetime"] != "" and row["dispatchtime"] != "" and row["totaltime"] != "", reader))
+                            and row["totalresponsetime"] != "" and row["dispatchtime"] != "" and row["totaltime"] != "", callsDict))
 
 avgResponse = reduce(lambda val, dict: val + float(dict["totalresponsetime"]), filteredCalls, 0)
-print("Response time avg: " + str(avgResponse/len(filteredCalls)))
+#print("Response time avg: " + str(avgResponse/len(filteredCalls)))
 avgDispatch = reduce(lambda val, dict: val + float(dict["dispatchtime"]), filteredCalls, 0)
-print("Response time avg: " + str(avgDispatch/len(filteredCalls)))
+#print("Dispatch time avg: " + str(avgDispatch/len(filteredCalls)))
 avgTotal = reduce(lambda val, dict: val + float(dict["totaltime"]), filteredCalls, 0)
-print("Response time avg: " + str(avgTotal/len(filteredCalls)))
+#print("Total time avg: " + str(avgTotal/len(filteredCalls)))
+
+neighborhoods = []
+for hood in filteredCalls:
+    if hood["neighborhood"] not in neighborhoods:
+        neighborhoods.append(hood["neighborhood"])
+#print(neighborhoods)
+
+jsonOut = []
+for hood in neighborhoods: 
+    #print(hood)
+    #get subset of dicts of each neighborhood
+    currentHoodGroup = list(filter(lambda row: row["neighborhood"] == hood, filteredCalls))
+
+    hoodResponse = reduce(lambda val, dict: val + float(dict["totalresponsetime"]), currentHoodGroup, 0)
+    #print("Response time avg: " + str(hoodResponse/len(filteredCalls)))
+    hoodDispatch = reduce(lambda val, dict: val + float(dict["dispatchtime"]), currentHoodGroup, 0)
+    #print("Response time avg: " + str(hoodDispatch/len(filteredCalls)))
+    hoodTotal = reduce(lambda val, dict: val + float(dict["totaltime"]), currentHoodGroup, 0)
+    #print("Response time avg: " + str(hoodTotal/len(filteredCalls)))
+    jsonOut.append({"neighborhood": hood, "Response time avg": hoodResponse/len(currentHoodGroup), 
+                "Dispatch time avg": hoodDispatch/len(currentHoodGroup), "Total time avg": hoodTotal/len(currentHoodGroup)})
+
+jsonOut.append({"neighborhood": "Overall", "Response time avg": avgResponse/len(filteredCalls), 
+                "Dispatch time avg": avgDispatch/len(filteredCalls), "Total time avg": avgTotal/len(filteredCalls)})
+#print(jsonOut[0], type(jsonOut[0]))
+with open("hoodOut.json", "w") as outfile:
+    json.dump(jsonOut, outfile)
 
 
-""" #for i in range(len(calls)):
-#filteredCalls = filter(lambda dict: dict.get("neighborhood") if dict.get("neighborhood") != "" else "" for dict in calls) 
-res = list(filter(None, ({key : val for key, val in dict.items() if val} for dict in calls)))
+calls.close()
 
-print(res[0])
-#filteredCalls = filter(lambda zip_code: zip_code != "", filteredCalls)
-
-total_time = reduce(lambda val, dict: )
-
-print(total_time/len(list(calls)))
-
-print(type(filteredCalls))
-print(list(filteredCalls[0])) """
-#print(calls[1])
-#print(calls[2])"""
